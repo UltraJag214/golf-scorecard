f749dbdb8aad98991aa38273895f1b979520df9f
UltraJag214 <hayden.gibbons8920@stu.mtec.edu>
2020-11-20 20:50:54 -0700

    added lock out, and support for buttons

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
 var courses;
-var courseData = {} ;
+var courseData = {};
 var players = [];
 var selectedPlayerCount;
+var parTotal;
+
+const endGame = {
+    YouSuck: 'Better luck next time',
+    YouRock: 'On to the PGA',
+    YouAverage: 'Right On Par'
+}
 
 $(document).ready(function () {
     onLoad();
@@ -28,7 +35,7 @@ function populateSelectDropdown() {
     getCourseData(courses.courses[0].id);
 }
 
-function getCourseData(id){
+function getCourseData(id) {
     $.ajax(`https://golf-courses-api.herokuapp.com/courses/${id}`)
         .done(function (returnData) {
             courseData[id] = returnData
@@ -40,7 +47,7 @@ function getCourseData(id){
         })
 }
 
-function populateDifficulty(data){
+function populateDifficulty(data) {
     $('#difficulty').empty();
     data.holes[0].teeBoxes.forEach(box => {
         var name = box.teeType.replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase());
@@ -48,16 +55,28 @@ function populateDifficulty(data){
     })
     // populate first element
     populateCourseData(data.holes[0].teeBoxes[0].teeTypeId)
+    sumParValues()
+    console.log(parTotal)
 }
 
-function populateCourseData(difficultyId){
+function sumParValues() {
+    var sum = 0;
+    $(".parItems").each(function () {
+        if ($(this).text() != null && $(this).text() != undefined && $(this).text() != '') {
+            sum += parseInt($(this).text());
+        }
+    });
+    parTotal = sum;
+}
+
+function populateCourseData(difficultyId) {
     var courseId = $('#chooseCourse').val();
     var courseHoles = courseData[courseId].data.holes
     var i = 0;
     courseHoles.forEach(hole => {
         var selectedDifficultyData;
         hole.teeBoxes.forEach(teeBox => {
-            if(teeBox.teeTypeId == difficultyId){
+            if (teeBox.teeTypeId == difficultyId) {
                 selectedDifficultyData = teeBox;
             }
         })
@@ -68,7 +87,15 @@ function populateCourseData(difficultyId){
     })
 }
 
-function addPlayerIfPossible(name){
+function addPlayerIfPossible(name) {
+    if(name == ""){
+        $("#banner").text("You Must Enter A Valid Name")
+        $("#banner").removeClass("hide")
+        return;
+    }
+    else{
+        $("#banner").addClass("hide")
+    }
     var index = players.length;
     players[index] = name;
     $(`#playerName${index}-0`).text(name);
@@ -78,16 +105,16 @@ function addPlayerIfPossible(name){
     $(`#player${index}-1`).removeClass("hide");
     $(`#player${index}-2`).removeClass("hide");
     $('#newUser').val('');
-    if(index == (selectedPlayerCount - 1)){
+    if (index == (selectedPlayerCount - 1)) {
         $('#newUserBtn').prop('disabled', true);
         $('#newUser').prop('disabled', true);
     }
 }
 
-function sumValue(elementClass, elementTotal){
+function sumValue(elementClass, elementTotal) {
     var sum = 0;
-    $(elementClass).each(function(){
-        if($(this).val() != null && $(this).val() != undefined && $(this).val() != ''){
+    $(elementClass).each(function () {
+        if ($(this).val() != null && $(this).val() != undefined && $(this).val() != '') {
             console.log(parseInt($(this).val()))
             sum += parseInt($(this).val());
         }
@@ -95,84 +122,108 @@ function sumValue(elementClass, elementTotal){
     $(elementTotal).text(sum);
 }
 
-function updateTotal(inScore, outScore, destination){
+function updateTotal(inScore, outScore, destination) {
     var inScoreValue = parseInt($(inScore).text()) || 0;
     var outScoreValue = parseInt($(outScore).text()) || 0;
 
     $(destination).text(inScoreValue + outScoreValue)
 }
 
+function calculateResults() {
+    for (let index = 0; index < selectedPlayerCount; index++) {
+        if ($(`#player${index}Score`).text() > parTotal) {
+            $(`#player${index}Outcome`).text(endGame.YouSuck)
+        }
+        else if ($(`#player${index}Score`).text() == parTotal) {
+            $(`#player${index}Outcome`).text(endGame.YouAverage)
+        }
+        else {
+            $(`#player${index}Outcome`).text(endGame.YouRock)
+        }
+    }
+}
+
 $("#chooseCourse").change(function () {
-    var selectId = $( this ).val()
+    var selectId = $(this).val()
     getCourseData(selectId);
 });
 
 $("#difficulty").change(function () {
-    var difficultyId = $( this ).val()
+    var difficultyId = $(this).val()
     console.log(difficultyId)
     populateCourseData(difficultyId);
 });
 
 $("#playerCount").change(function () {
-    var count = $( this ).val()
+    var count = $(this).val()
     var oldValue = selectedPlayerCount;
     selectedPlayerCount = count;
-    if(oldValue != selectedPlayerCount){
+    if (oldValue != selectedPlayerCount) {
         $('#newUserBtn').prop('disabled', false);
         $('#newUser').prop('disabled', false);
     }
-
-    console.log(selectedPlayerCount)
 });
 
-$("#newUserBtn").click(function() {
+$("#newUserBtn").click(function () {
     addPlayerIfPossible($('#newUser').val())
-  });
+});
 
-$(".player0-0").change(function(){
+$(".player0-0").change(function () {
     sumValue('.player0-0', "#player0Total0")
     updateTotal("#player0Total0", "#player0Total1", "#player0Score")
 })
 
-$(".player1-0").change(function(){
+$(".player1-0").change(function () {
     sumValue('.player1-0', "#player1Total0")
     updateTotal("#player1Total0", "#player1Total1", "#player1Score")
 })
 
-$(".player2-0").change(function(){
+$(".player2-0").change(function () {
     sumValue('.player2-0', "#player2Total0")
     updateTotal("#player2Total0", "#player2Total1", "#player2Score")
 })
 
-$(".player3-0").change(function(){
+$(".player3-0").change(function () {
     sumValue('.player3-0', "#player3Total0")
     updateTotal("#player3Total0", "#player3Total1", "#player3Score")
 })
 
-$(".player0-1").change(function(){
+$(".player0-1").change(function () {
     sumValue('.player0-1', "#player0Total1")
     updateTotal("#player0Total0", "#player0Total1", "#player0Score")
 })
 
-$(".player1-1").change(function(){
+$(".player1-1").change(function () {
     sumValue('.player1-1', "#player1Total1")
     updateTotal("#player1Total0", "#player1Total1", "#player1Score")
 })
 
-$(".player2-1").change(function(){
+$(".player2-1").change(function () {
     sumValue('.player2-1', "#player2Total1")
     updateTotal("#player2Total0", "#player2Total1", "#player2Score")
 })
 
-$(".player3-1").change(function(){
+$(".player3-1").change(function () {
     sumValue('.player3-1', "#player3Total1")
     updateTotal("#player3Total0", "#player3Total1", "#player3Score")
 })
 
-$('#newUser').keydown(function(event){ 
-    var keyCode = (event.keyCode ? event.keyCode : event.which);   
+$('#newUser').keydown(function (event) {
+    var keyCode = (event.keyCode ? event.keyCode : event.which);
     if (keyCode == 13) {
         $('#newUserBtn').trigger('click');
     }
 });
-  
+
+$("#startGame").click(function () {
+    if ($("#startGame").text() == "New Game") {
+        location.reload();
+    }
+    if ($("#startGame").text() == "End Game") {
+        $("#startGame").text("New Game")
+        calculateResults()
+        return;
+    }
+    $(".thingsToLock").prop("disabled", true)
+    $("#startGame").text("End Game")
+});
